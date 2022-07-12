845410e3b966da1121da5db5679905bf2a844504
artem.meniak@gmail.com <user@address.com>
2022-07-12 14:54:59 +0300

    Added a function for generating Fibonacci sequence of given length.

--- Homework1/Task/Utils.cs
+++ Homework1/Task/Utils.cs
@@ -15,5 +15,20 @@ namespace Homework1.Task
         {
             return Enumerable.Range(1, value).Aggregate((a, b) => a * b);
         }
+
+        /// <summary>
+        /// Generates a Fibonacci sequence with given length.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static List<long> GetFibonacci(int length)
+        {
+            var seq = new List<long>();
+            Enumerable.Range(0, length)
+                .ToList()
+                .ForEach(num => 
+                    seq.Add(num <= 1 ? 1 : seq[num - 1] + seq[num - 2]));
+            return seq;
+        }
     }
 }
