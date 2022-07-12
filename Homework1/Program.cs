59d897a0ff18ec96acaeff09041774e72d65a4c7
artem.meniak@gmail.com <user@address.com>
2022-07-12 14:33:13 +0300

    Added function for calculating factorial using LINQ.

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
+using Homework1.Task;
+
 namespace Homework1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine(Utils.GetFactorial(9));
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message + "\nTry passing another argument!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\nSomething unexpected happened!");
+            }
         }
     }
 }
---
+++ Homework1/Task/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework1.Task
+{
+    public static class Utils
+    {
+
+        /// <summary>
+        /// Calculates factorial value for given number.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Final factorial value.</returns>
+        public static long GetFactorial(int value)
+        {
+            return Enumerable.Range(1, value).Aggregate((a, b) => a * b);
+        }
+    }
+}
